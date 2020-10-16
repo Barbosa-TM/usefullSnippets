815be24f239dc3c122f870cb613d2a4c6fc17e78
tm-barbosa <56256420+tm-barbosa@users.noreply.github.com>
2020-10-16 16:56:20 +0100

    Create GenRanger.py

---
+++ GenRanger.py
@@ -0,0 +1,18 @@
+def GenRanger(end, start=0, step=1):    
+    """   
+    Similar in use to standard python range but without generating a list.
+    """
+    #checking positive convergence
+    if end < start and step > 0:
+        pass
+    #checking negative convergence
+    elif end > start and step < 0:
+        pass
+    #yielding
+    else:
+        index = start
+        niter = abs(end)
+        while niter > 0:
+            yield index
+            index += step
+            niter -= abs(step)
